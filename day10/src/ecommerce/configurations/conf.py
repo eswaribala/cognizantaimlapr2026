9c60ff53b2b582f3ffe8bd5429bd37e41c8a0774
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-04-24 17:19:12 +0530

    updated

---
+++ day10/src/ecommerce/configurations/conf.py
@@ -0,0 +1,18 @@
+#create mysql connection
+import os
+from dotenv import load_dotenv
+load_dotenv()
+class Config:
+    def __init__(self):
+        self.host = os.getenv("host")
+        self.port = os.getenv("port")
+        self.mysql_user = os.getenv("mysql_user")
+        self.mysql_password = os.getenv("mysql_password")
+        self.database = os.getenv("database")
+
+    def get_connection_string(self):
+        app_env = os.getenv("APP_ENV")
+        if app_env == "development":
+            return f"mysql+pymysql://{self.mysql_user}:{self.mysql_password}@{self.host}:{self.port}/{self.database}"
+        else:
+            raise ValueError("Invalid APP_ENV value. Expected 'development'.")
