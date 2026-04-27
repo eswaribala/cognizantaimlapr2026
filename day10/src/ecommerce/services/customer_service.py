55fa329911b5e687ca261bd74ff96de5945f797f
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-04-27 16:39:29 +0530

    updated

--- day10/src/ecommerce/services/customer_service.py
+++ day10/src/ecommerce/services/customer_service.py
@@ -9,13 +9,4 @@ class CustomerService(ABC):
     @abstractmethod
     def get_all_customers(self):
         pass
-    @abstractmethod
-    def get_customer_by_id(self, customer_id):
-        pass
-    @abstractmethod
-    def update_customer(self, customer_id, customer_data):
-        pass
-
-    @abstractmethod
-    def delete_customer(self, customer_id):
-        pass
+    
---
+++ day10/src/ecommerce/services/customer_service_impl.py
@@ -0,0 +1,32 @@
+#create customer service implementation class
+from ecommerce.repository.customer_repository_impl import CustomerRepositoryImpl
+
+from .customer_service import CustomerService
+
+class CustomerServiceImpl(CustomerService):
+    def __init__(self):
+        self.customer_repository = CustomerRepositoryImpl()
+
+    def to_customer_response(self, customers):
+        customer_responses = []
+        for customer in customers:
+            customer_response = {
+                "id": customer.id,
+                "first_name": customer.full_name.first_name,
+                "last_name": customer.full_name.last_name,
+                "email": customer.email,
+                "created_at": customer.created_at,
+                "updated_at": customer.updated_at
+            }
+            customer_responses.append(customer_response)
+        return customer_responses
+
+    def create_customer(self, customer_data):
+        #dto to entity and saving to database
+        customer = self.customer_repository.create_customer(customer_data)
+        return self.to_customer_response([customer])[0]
+
+    def get_all_customers(self):
+        #convert entities to dtos and return
+        customers = self.customer_repository.get_all_customers()
+        return self.to_customer_response(customers)
