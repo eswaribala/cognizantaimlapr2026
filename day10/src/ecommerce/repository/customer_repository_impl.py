21b5bb68bb05c6035fd130746285b74194565237
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-04-27 16:00:10 +0530

    updated

--- day10/src/ecommerce/repository/customer_repository_impl.py
+++ day10/src/ecommerce/repository/customer_repository_impl.py
@@ -3,6 +3,7 @@ from ecommerce.models.customer import Customer
 from ecommerce.models.full_name import FullName
 from ecommerce.repository.customer_repository import CustomerRepository
 from ecommerce.configurations.mysql_conn import MySQLConnection
+from datetime import datetime
 class CustomerRepositoryImpl(CustomerRepository): 
 
     def create_customer(self, customer_request):
@@ -16,7 +17,10 @@ class CustomerRepositoryImpl(CustomerRepository):
         customer=Customer(
             full_name=full_name,
             email=customer_request.email,
-            phone_number=customer_request.phone_number
+            password=customer_request.password,
+            created_at=datetime.now(),
+            updated_at=datetime.now()
+            
 
         )
         #insert customer into database
