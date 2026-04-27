836df5ea6862b4408a99cb25f99c26cd662632ca
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-04-27 17:44:44 +0530

    updated

--- day10/src/ecommerce/controllers/customer_controller.py
+++ day10/src/ecommerce/controllers/customer_controller.py
@@ -1,6 +1,8 @@
 
 from fastapi import APIRouter
 
+from ecommerce.dtos.customer_request import CustomerRequest
+from ecommerce.dtos.customer_response import CustomerResponse
 from ecommerce.services import customer_service
 from ecommerce.services.customer_service_impl import CustomerServiceImpl
 
@@ -10,11 +12,11 @@ router = APIRouter(prefix="/customers/v1.0", tags=["customers"])
 #connecting the service to the controller
 customer_service = CustomerServiceImpl()
 
-@router.post("/")
-def create_customer(customer_data: dict):
+@router.post("/", status_code=201, response_model=CustomerResponse)
+def create_customer(customer_data: CustomerRequest):
     return customer_service.create_customer(customer_data)
 
-@router.get("/")
+@router.get("/", status_code=200, response_model=list[CustomerResponse])
 def get_customers():
     return customer_service.get_all_customers()
 
