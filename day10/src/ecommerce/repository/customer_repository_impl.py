ab86b9c9eab95fd50458cedc738f2ccddf3e7d1d
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-04-27 15:58:34 +0530

    updated

---
+++ day10/src/ecommerce/repository/customer_repository_impl.py
@@ -0,0 +1,29 @@
+#create customer respository implementation
+from ecommerce.models.customer import Customer
+from ecommerce.models.full_name import FullName
+from ecommerce.repository.customer_repository import CustomerRepository
+from ecommerce.configurations.mysql_conn import MySQLConnection
+class CustomerRepositoryImpl(CustomerRepository): 
+
+    def create_customer(self, customer_request):
+        session=MySQLConnection.get_session()
+        #converting customer request to customer model
+        #converting full name request to full name model
+        full_name=FullName(
+            first_name=customer_request.first_name,
+            last_name=customer_request.last_name
+        )
+        customer=Customer(
+            full_name=full_name,
+            email=customer_request.email,
+            phone_number=customer_request.phone_number
+
+        )
+        #insert customer into database
+        session.add(customer)
+        #commit the transaction
+        session.commit()
+        #close the session
+        session.close()
+
+        return customer
