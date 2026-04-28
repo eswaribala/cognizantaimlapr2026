9dbc589999130d0b323f86dc1e07eeb3ee4839d5
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-04-28 17:14:29 +0530

    updated

--- day10/src/ecommerce/main.py
+++ day10/src/ecommerce/main.py
@@ -11,15 +11,17 @@ from ecommerce.configurations.mysql_conn import base, engine
 #  IMPORT MODELS FIRST (VERY IMPORTANT)
 from ecommerce.models.customer import Customer  
 from ecommerce.models.catalog import Catalog
+from ecommerce.models.product import Product
 #create all the tables in the database
 base.metadata.create_all(bind=engine)
 #make api call to the customer controller
 
-from ecommerce.controllers import customer_controller
+from ecommerce.controllers import customer_controller, product_controller
 from ecommerce.controllers import catalog_controller
 
 app.include_router(customer_controller.router)
 app.include_router(catalog_controller.router)
+app.include_router(product_controller.router)
 
 
 
