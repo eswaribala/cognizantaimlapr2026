45d9709ac2238d9a73d92c92fbf26b585543d02e
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-04-30 14:54:19 +0530

    updated

--- day11/src/bankingapp/configurations/conf.py
+++ day11/src/bankingapp/configurations/conf.py
@@ -1,6 +1,8 @@
 #create confoguration file for the application
 import os
+
 from dotenv import load_dotenv
+import hvac
 env_path = os.path.join(os.path.dirname(__file__), '..', '.env')
 load_dotenv(dotenv_path=env_path)
 
@@ -12,8 +14,19 @@ class Config:
     
     def get_mongo_uri(self):
         app_env=os.getenv('APP_ENV')
+        root_token=os.getenv('root_token')
         if app_env == 'development':
-            return os.getenv('mongo_uri')
+            client = hvac.Client(
+                url="http://localhost:8200",
+                token=root_token       # dev token
+            )
+            response = client.secrets.kv.v1.read_secret(
+                path="mongoatlassecret",
+                mount_point="secret"
+            )
+            mongo_uri = response["data"]["uri"]
+            return mongo_uri
+            #return os.getenv('mongo_uri')
         else:
             raise ValueError("Invalid APP_ENV value. Expected 'development'.")
             
