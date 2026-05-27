c1ce5f7b149f04ee7239b022eab7ccefc975a00a
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-05-27 12:39:49 +0530

    updated

--- ml2/src/orderapp/main.py
+++ ml2/src/orderapp/main.py
@@ -1,11 +1,52 @@
 from fastapi import FastAPI
+import consul
+from contextlib import asynccontextmanager
+from orderapp.configurations.config import CONSUL_HOST, CONSUL_PORT, SERVICE_NAME_1, SERVICE_ID_1, SERVICE_HOST_1, SERVICE_NAME_1, SERVICE_PORT_1, SERVICE_NAME_2, SERVICE_ID_2, SERVICE_HOST_2, SERVICE_PORT_2
+# --------------------------------
+# Lifespan (startup + shutdown)
+# --------------------------------
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+
+    # startup
+    c = consul.Consul(
+        host=CONSUL_HOST,
+        port=CONSUL_PORT
+    )
+
+    c.agent.service.register(
+        name=SERVICE_NAME_1,
+        service_id=SERVICE_ID_1,
+        address=SERVICE_HOST_1,
+        port=SERVICE_PORT_1,
+        check={
+            "http": f"http://{SERVICE_HOST_1}:{SERVICE_PORT_1}/health",
+            "interval": "10s",
+            "timeout": "5s"
+        }
+    )
+
+    print("✅ Order service registered with Consul")
+
+    yield
+
+    # shutdown
+    c.agent.service.deregister(SERVICE_ID_1)
+
+    print("❌ Order service deregistered")
+
+
 
 app = FastAPI(
     title="🛒 E-commerce API",
     description="API for managing e-commerce operations",
-    version="1.0.0"
+    version="1.0.0",lifespan=lifespan
 )
-
+@app.get("/health")
+def health():
+    return {
+        "status": "UP"
+    }
 from orderapp.configurations.mysql_conf import engine, base
 from orderapp.models.order import Order
 
